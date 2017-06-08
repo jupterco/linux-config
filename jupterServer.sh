0da4260a4e3fb6c5daf44274b5a8d64d6b494c4f
Daniel Ikenaga <daniel@concatenando.com>
2017-06-08 15:34:00 -0300

    Atualiza mysql

--- jupterServer.sh
+++ jupterServer.sh
@@ -2,6 +2,8 @@
 
 # Debian Jessie
 
+sudo apt-get update
+
 echo -------------------------------------------------------------------
 echo Install git [1/0]
 echo -------------------------------------------------------------------
@@ -35,3 +37,27 @@ if [ $option -eq 1 ] ; then
 	sudo apt-get install -y php7.0
 fi
 
+
+echo -------------------------------------------------------------------
+echo Install TensorFlow [1/0]
+echo -------------------------------------------------------------------
+
+read option
+if [ $option -eq 1 ] ; then
+	sudo apt-get install python3-numpy python3-dev python3-pip python3-wheel
+	cd ~/
+	git clone https://github.com/tensorflow/tensorflow 
+	cd tensorflow
+fi
+
+sudo apt-get update
+
+echo -------------------------------------------------------------------
+echo Install MySQL [1/0]
+echo -------------------------------------------------------------------
+
+read option
+if [ $option -eq 1 ] ; then
+	sudo apt-get install mysql-server
+	sudo mysql_secure_installation
+fi
